410737b594409fd0d0085b3c408b2461f21d7782
Matthew McFarland <mmcfarland@azavea.com>
2017-09-01 17:56:20 -0400

    Setup to allow Chalice deploys

--- src/tiler/app.py
+++ src/tiler/app.py
@@ -1,6 +1,6 @@
 from chalice import Chalice, Response
 import logging
-from tile import render
+from chalicelib import tile
 
 app = Chalice(app_name='tiler')
 app.log.setLevel(logging.DEBUG)
@@ -12,8 +12,8 @@ def index():
 
 
 @app.route('/tile/{z}/{x}/{y}')
-def tile(z, x, y):
-    return Response(body=render(app, int(z), int(x), int(y)),
+def render(z, x, y):
+    return Response(body=tile.render(app, int(z), int(x), int(y)),
                     status_code=200,
                     headers={
                         'Content-Type': 'image/png'
