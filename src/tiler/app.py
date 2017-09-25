0f03acfd6c354c3ef5276c59ffd1eae5bb5651da
Matthew McFarland <mmcfarland@azavea.com>
2017-09-25 10:19:37 -0400

    Safely get query params
    Chalice behaves differently between local and lambda in terms of query
    string access.  In lambda, if none are provided, the object is None, not
    an empty dict.

--- src/tiler/app.py
+++ src/tiler/app.py
@@ -1,6 +1,8 @@
 from chalice import Chalice, Response, ChaliceViewError
-import logging
 from chalicelib import tile
+
+import logging
+import traceback
 
 app = Chalice(app_name='lambnik-tiler')
 app.log.setLevel(logging.DEBUG)
@@ -16,20 +18,19 @@ def index():
 def render_grid(z, x, y):
     app.log.debug('grid')
     try:
-        inlet = app.current_request.query_params.get('type', None)
+        inlet = get_filter(app.current_request)
         grd = tile.grid(int(z), int(x), int(y), inlet)
 
         return grd
     except Exception, e:
-        app.log.error(e)
-        return ChaliceViewError(e.message)
+        return handle_exception(e)
 
 
 @app.route('/tile/{z}/{x}/{y}')
 def render_tile(z, x, y):
     app.log.debug('tile')
     try:
-        inlet = app.current_request.query_params.get('type', None)
+        inlet = get_filter(app.current_request)
         img = tile.image(int(z), int(x), int(y), inlet)
 
         return Response(body=img, status_code=200,
@@ -37,10 +38,22 @@ def render_tile(z, x, y):
                             'Content-Type': 'image/png'
                         })
     except Exception, e:
-        app.log.error(e)
-        return ChaliceViewError(e.message)
+        return handle_exception(e)
 
 
 @app.route('/favicon.ico')
 def favicon():
     pass
+
+
+def get_filter(req):
+    if req.query_params:
+        return req.query_params.get('type', None)
+    else:
+        return None
+
+def handle_exception(e):
+    app.log.error(e)
+    app.log.error(traceback.format_exc())
+    return ChaliceViewError(e.message)
+
